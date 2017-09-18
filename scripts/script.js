0f615776ea609138536807fb1d98c64d09304e6d
Samuel Newhouse <samuel_newhouse@yahoo.com>
2017-09-17 20:38:56 -0500

    started copying in AI program

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,155 @@
 $(function () {
 	var boardTracker = (function () {
+
+		// https://mostafa-samir.github.io/Tic-Tac-Toe-AI/
+		// 
+		// Represents a state in the game
+		// @param old [State]: old state to intialize the new state
+		var State = function (old) {
+			this.turn = "";
+			this.oMovesCount = 0;
+			this.result = "still running";
+			this.board = [];
+
+			if (typeof old !== "undefined") {
+				var len = old.board.length;
+				this.board = new Array(len);
+				for (var itr = 0; itr < len; itr++) {
+					this.board[itr] = old.board[itr];
+				}
+
+				this.oMovesCount = old.oMovesCount;
+				this.result = old.result;
+				this.turn = old.turn;
+			}
+
+			this.advanceTurn = function () {
+				this.turn = this.turn === "X" ? "O" : "X";
+			}
+
+			this.emptyCells = function () {
+				var indxs = [];
+				for (var itr = 0; itr < 9; itr++) {
+					if (this.board[itr] === "") {
+						indxs.push(itr);
+					}
+				}
+				return indxs;
+			}
+
+			this.isTerminal = function () {
+				var B = this.board;
+
+				//check rows
+				for (var i = 0; i <= 6; i = i + 3) {
+					if (B[i] !== "" && B[i] === B[i + 1] && B[i + 1] == B[i + 2]) {
+						this.result = B[i] + "-won"; //update the state result
+						return true;
+					}
+				}
+
+				//check columns
+				for (var i = 0; i <= 2; i++) {
+					if (B[i] !== "" && B[i] === B[i + 3] && B[i + 3] === B[i + 6]) {
+						this.result = B[i] + "-won"; //update the state result
+						return true;
+					}
+				}
+
+				//check diagonals
+				for (var i = 0, j = 4; i <= 2; i = i + 2, j = j - 2) {
+					if (B[i] !== "" && B[i] == B[i + j] && B[i + j] === B[i + 2 * j]) {
+						this.result = B[i] + "-won"; //update the state result
+						return true;
+					}
+				}
+
+				var available = this.emptyCells();
+
+				if (available.length == 0) {
+					//the game is draw
+					this.result = "draw"; //update the state result
+					return true;
+				}
+				else {
+					return false;
+				}
+			} // isTerminal
+		}	// State
+
+		var AI = function (level) {
+			var levelOfIntelligence = level;
+			var game = {};
+
+			function minimaxValue(state) {
+
+			}
+
+			function takeABlindMove(turn) { // @param turn [String]: the player to play, either X or O
+
+			}
+
+			function takANoviceMove(turn) { // @param turn [String]: the player to play, either X or O
+
+			}
+
+			function takeAMasterMove(turn) { // @param turn [String]: the player to play, either X or O
+
+			}
+
+			this.plays = function (_game) {
+				game = _game;
+			}
+
+			this.notify = function (turn) {
+				switch (levelOfIntelligence) {
+					case "blind": takeABlindMove(turn); break;
+					case "novice": takeANoviceMove(turn); break;
+					case "master": takeAMasterMove(turn); break;
+				}
+			} // notify
+		} // AI
+
+		var AIAction = function (pos) {
+			this.movePosition = pos;
+			this.minimaxVal = 0;
+
+			this.applyTo = function (state) {
+				var next = new.State(state);
+
+				next.board[this.movePosition] = state.turn;
+
+				if (state.turn === "O")
+					next.oMovesCount++;
+
+				next.advanceTurn();
+
+				return next;
+			}
+
+		}
+
+		AIAction.ASCENDING = function (firstAction, secondAction) {
+			if (firstAction.minimaxVal < secondAction.minimaxVal)
+				return -1; //indicates that firstAction goes before secondAction
+			else if (firstAction.minimaxVal > secondAction.minimaxVal)
+				return 1; //indicates that secondAction goes before firstAction
+			else
+				return 0; //indicates a tie
+		}
+
+		AIAction.DESCENDING = function (firstAction, secondAction) {
+			if (firstAction.minimaxVal > secondAction.minimaxVal)
+				return -1; //indicates that firstAction goes before secondAction
+			else if (firstAction.minimaxVal < secondAction.minimaxVal)
+				return 1; //indicates that secondAction goes before firstAction
+			else
+				return 0; //indicates a tie
+		}
+		
 		var gameOver = false;
 		var playerTurn = "X";
+		var humanSide = "X";
 		var turnNum = 1;
 		var cells = {
 			"top-left": "", "top-middle": "", "top-right": "",
@@ -9,9 +157,22 @@ $(function () {
 			"bottom-left": "", "bottom-middle": "", "bottom-right": ""
 		};
 
+		function getBoardState() { // Get copy of board state to play with, so cells object isn't changed.
+			/*
+			var boardState = {
+				"top-left":    cells["top-left"]   ,    "top-middle": cells["top-middle"]   ,    "top-right": cells["top-right"],
+				"middle-left": cells["middle-left"], "middle-middle": cells["middle-middle"], "middle-right": cells["middle-right"],
+				"bottom-left": cells["bottom-left"], "bottom-middle": cells["bottom-middle"], "bottom-right": cells["bottom-right"]
+			}
+			*/
+			return extend({}, cells);
+		}
+
 		function endTurn() {
 			turnNum++;
 			playerTurn = playerTurn === "X" ? "O" : "X";
+			if (playerTurn !== humanSide)
+				doComputerTurn();
 		}
 
 		function resetBoard() {
@@ -30,6 +191,48 @@ $(function () {
 
 		function playerSelect() {
 			$("#popup-button").show();
+		}
+
+		function doPlacement(id) {
+			cells[id] = playerTurn;
+			$("#" + id + " > div").html(playerTurn);
+			testWin();
+			endTurn();
+		}
+
+		function evalHumanWin() {
+		}
+
+		function evalComputerWin() {
+
+		}
+
+		function doComputerTurn() {
+			console.log("doComputerTurn: " + turnNum);
+			switch (turnNum) {
+				case 1:
+					doPlacement("top-left");
+					break;
+				case 2:
+					if (cells["middle-middle"] === "")
+						doPlacement("middle-middle");
+					else
+						doPlacement("top-left");
+					break;
+				case 3:
+					if (cells["middle-middle"] === "") {
+						if (cells["top-middle"] === "" && cells["top-right"] === "")
+							doPlacement("top-right");
+						else
+							doPlacement("bottom-left");
+					}
+					else {
+						doPlacement("bottom-right");
+					}
+					break;
+				case 4:
+					break;
+			}
 		}
 
 		function doWin() {
@@ -82,13 +285,16 @@ $(function () {
 			// *** Public methods and variables. ***
 			tryPlacement: function (id) {
 				if (!gameOver && cells[id] === "") {
-					cells[id] = playerTurn;
-					$("#" + id + " > div").html(playerTurn);
-					testWin();
-					endTurn();
+					doPlacement(id);
 				}
-				else if (id === "popup-button") {
+				else if (id === "play-x") {
+					humanSide = "X";
 					resetBoard();
+				}
+				else if (id === "play-o") {
+					humanSide = "O";
+					resetBoard();
+					doComputerTurn();
 				}
 			}
 		}; // return		
@@ -100,5 +306,5 @@ $(function () {
 	}
 	$("#div-outer div").each(function () {
 		$(this).click(handleClicks);
-	});	
+	});
 });
